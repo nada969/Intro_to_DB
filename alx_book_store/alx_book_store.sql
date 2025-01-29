c57b8ba867b869123ffb5ee8c1ccee457cb9e43a
nada969 <nadamousa969@gmail.com>
2025-01-29 21:15:37 +0200

    add alx_book_store

---
+++ alx_book_store/alx_book_store.sql
@@ -0,0 +1,45 @@
+-- CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors (
+author_id INT,
+PRIMARY Key (author_id),
+author_name VARCHAR(215) 
+);
+
+CREATE TABLE Books (
+book_id INT,
+author_id INT,
+title VARCHAR(130),
+price DOUBLE,
+publication_date DATE,
+PRIMARY Key (book_id),
+Foreign Key (author_id) REFERENCES Authors (author_id)
+);
+
+
+CREATE TABLE Customers (
+customer_id INT,
+PRIMARY Key (customer_id ),
+customer_name VARCHAR(215),
+email VARCHAR(215),
+address TEXT
+);
+
+CREATE TABLE Orders (
+Order_id INT,
+customer_id INT,
+PRIMARY Key (Order_id ),
+Foreign Key (customer_id) REFERENCES Customers (customer_id),
+order_date DATE
+);
+
+CREATE TABLE Order_Details (
+detail_id INT,
+Order_id INT,
+book_id INT,
+PRIMARY Key (detail_id),
+Foreign Key (Order_id) REFERENCES Orders (Order_id),
+Foreign Key (book_id) REFERENCES Books (book_id),
+quantity DOUBLE
+);
