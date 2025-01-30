5542d1cc85f274c8f1d858ebb1f7c559201c0f5d
nada969 <nadamousa969@gmail.com>
2025-01-30 05:10:10 +0200

    add task2

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,21 +1,29 @@
 # from mysql.connector import connect , Error
 import mysql.connector
+import os
 try:
-    MY_DB = mysql.connector.connect(
+
+    with mysql.connector.connect(       
         host="localhost",
         user="Nada",
+        # user = os.environ.get('DB_USER'),
+        # passwod =  os.environ.get('DB_PASS'), 
         password="0752",
         database="alx_book_store"
-    )
+    ) as MY_DB:
     # with connect(
     #     host="localhost",
     #     user="Nada",
     #     password="0752",
     #     database="alx_book_store"
     # ) as connection: 
-    print(f'Database \'{MY_DB.database}\' created successfully!');
+        # cursor = MY_DB.cursor()
+        # print(f'Database \'{MY_DB.database}\' created successfully!')
+        create_db_query = "CREATE DATABASE IF NOT EXISTS alx_book_store"   ####sql code
+        with MY_DB.cursor() as cursor:
+            cursor.execute(create_db_query)
 
-except Exception as e:
+except mysql.connector.Error as e:
     print(e)
 
 # MY_DB = mysql.connector.connect(
