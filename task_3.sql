5abab5e58eb8857c976b26b774bfaff33e64e17c
nada969 <nadamousa969@gmail.com>
2025-01-30 05:20:35 +0200

    add task3

--- task_3.sql
+++ task_3.sql
@@ -1,7 +1,7 @@
 import alx_book_store
-
-SELECT * FROM Authors;
-SELECT * FROM Books;
-SELECT * FROM Customers;
-SELECT * FROM Orders;
-SELECT * FROM Order_Details;
+SHOW TABLES FROM alx_book_store
+-- SELECT * FROM Authors;
+-- SELECT * FROM Books;
+-- SELECT * FROM Customers;
+-- SELECT * FROM Orders;
+-- SELECT * FROM Order_Details;
