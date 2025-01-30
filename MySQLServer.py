a3db53961ef66ce5e6de1fe91b8898d63a84c0f9
nada969 <nadamousa969@gmail.com>
2025-01-30 04:27:39 +0200

    add python script

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,26 @@
-import mysql.connector
+from mysql.connector import connect , Error
 
-MY_DB = mysql.connector.connect(
-    host="localhost",
-    user="Nada",
-    password="0752",
-    database="alx_book_store"
-);
-while MY_DB.get_server_info():
-    try:
-        print(f'Database \'{MY_DB.database}\' created successfully!')
-        break
-    except Exception as E:
-        print(E)
+try:
+    with connect(
+        host="localhost",
+        user="Nada",
+        password="0752",
+        database="alx_book_store"
+    ) as connection: 
+        print(f'Database \'{connection.database}\' created successfully!');
+
+except Error as e:
+    print(e)
+
+# MY_DB = mysql.connector.connect(
+#     host="localhost",
+#     user="Nada",
+#     password="0752",
+#     database="alx_book_store"
+# );
+# while MY_DB.get_server_info():
+#     try:
+#         print(f'Database \'{MY_DB.database}\' created successfully!')
+#         break
+#     except Exception as E:
+#         print(E)
