498c6f9caddec99303fbb5af38b99d9bd2256f18
nada969 <nadamousa969@gmail.com>
2025-01-30 05:40:47 +0200

    add task5

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,3 @@
 SELECT COLUMN_NAME, COLUMN_TYPE
 from INFORMATION_SCHEMA.COLUMNS
-where TABLE_SCHEMA = alx_book_store , TABLE_NAME = Books
+where TABLE_SCHEMA = 'alx_book_store' , TABLE_NAME = 'Books'
